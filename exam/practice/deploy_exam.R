29be46cd163d865b3ce3fb6b900e82ef835297e2
Kyle M. Lang <k.m.lang@uu.nl>
2021-12-17 12:42:24 +0100

    trying to fix bugs caused by Windows being a stupid ass

--- exam/practice/deploy_exam.R
+++
@@ -1,8 +0,0 @@
-### Title:    Deploy Practice Exam as Shiny App
-### Author:   Kyle M. Lang
-### Created:  2021-12-15
-### Modified: 2021-12-15
-
-library(rsconnect)
-
-rsconnect::deployApp("./practice_exam.Rmd")
